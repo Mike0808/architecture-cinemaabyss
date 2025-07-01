b8d76086cf6fcbae4b1bcf793b306e0fda166bc5
Mikhail Kuznetsov <mikhailkuznetsov@MacBook-Pro-Mikhail.local>
2025-07-01 12:56:11 +0500

    fix.proxy service - refactor. kube.env for npm tests for port 30080

--- src/microservices/proxy/src/main.rs
+++ src/microservices/proxy/src/main.rs
@@ -70,13 +70,16 @@ async fn handle_request(
         return Ok(Response::new(Body::from("OK")));
     }
 
-    let backend = if req.uri().path().starts_with("/api/users") {
-        &flags.monolith
-    } else if flags.use_movies() {
-        &flags.movies
+    let backend = if req.uri().path() == "/api/movies" {
+        if flags.use_movies() {
+            &flags.movies
+        } else {
+            &flags.monolith
+        }
     } else {
         &flags.monolith
     };
+    
     info!("Choosed backend: {}", backend);
 
     let (parts, body) = req.into_parts();
